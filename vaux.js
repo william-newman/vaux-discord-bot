02a9fff56ad58716fefc29e401d44a339a50a567
William Newman <38147848+william-newman@users.noreply.github.com>
2021-02-07 21:00:48 -0700

    Added functionality for second server

--- vaux.js
+++ vaux.js
@@ -5,7 +5,7 @@ import Spotify from "node-spotify-api";
 
 const spotifyAPI = new Spotify({
   id: "<your spotify client id>",
-  secret: "<your spotify client secret>"
+  secret: "<your spotify client secret>",
 });
 const client = new Discord.Client();
 const channels = client.channels;
@@ -44,7 +44,7 @@ client.on("ready", () => {
     client.user.setActivity("Sad Hour 💀");
   } else {
     // client.user.setActivity("directive 6 protocol", { type: "LISTENING" });
-    client.user.setActivity("sad stuff", { type: "LISTENING" });
+    client.user.setActivity("the voice of the people", { type: "LISTENING" });
     // client.user.setActivity("Follow The Leader");
   }
 
@@ -78,152 +78,171 @@ client.on("presenceUpdate", (presence) => {
 // Function to be triggered on any message to any server the bot is in
 client.on("message", (receivedMessage) => {
   const incomingM = receivedMessage.content.toLowerCase();
-  const bellaEmote = receivedMessage.guild.emojis.cache.get(
-    "692452430306082918"
-  );
-  const chaseBabyEmote = receivedMessage.guild.emojis.cache.get(
-    "677646228716257280"
-  );
-  const daddyEmote = receivedMessage.guild.emojis.cache.get(
-    "684533234184486913"
-  );
 
-  if (receivedMessage.author.username.match("ChaseDunton")) {
-    const chaseReactProbability = Math.floor(Math.random() * 10);
-    if (chaseReactProbability <= 2) {
-      receivedMessage.react(chaseBabyEmote);
-    }
-  }
-
+  // Bot will not repond to its own messages
   if (receivedMessage.author == client.user) {
     return;
-  } else if (receivedMessage.channel.id !== "755615150299676752") {
+  }
+
+  // Generic commands
+  if (receivedMessage.guild.id !== "278406764846907392") {
+    // Vaux says "Hi"
+    if (incomingM.startsWith("vx say hi")) {
+      receivedMessage.channel.send(
+        "Yo yo what up " + receivedMessage.author.toString() + "!",
+        { tts: true }
+      );
+      localCommandCountDataWriteService("sayHi");
+    }
+
+    // Tells presence message
+    if (
+      incomingM.startsWith("vx what are you doing") ||
+      incomingM.startsWith("vx wyd")
+    ) {
+      const activities = client.user.presence.activities;
+      let botActivities = [];
+      activities.forEach((activity) => {
+        botActivities.push(
+          activity.type.toLocaleLowerCase() + " to " + activity.name
+        );
+      });
+      receivedMessage.channel.send(botActivities.toLocaleString());
+    }
+  }
+
+  // Bat Country server only
+  if (receivedMessage.guild.id === "735527963306492017") {
+    // no server specific commands at current
     return;
   }
 
-  if (receivedMessage.author.username.match("LN")) {
-    const tadaReactProbability = Math.floor(Math.random() * 10);
-    if (tadaReactProbability === 0) {
-      receivedMessage.react("🎉");
+  // Catalitos server only
+  if (receivedMessage.guild.id === "278406764846907392") {
+    const bellaEmote = receivedMessage.guild.emojis.cache.get(
+      "692452430306082918"
+    );
+    const chaseBabyEmote = receivedMessage.guild.emojis.cache.get(
+      "677646228716257280"
+    );
+    const daddyEmote = receivedMessage.guild.emojis.cache.get(
+      "684533234184486913"
+    );
+
+    if (receivedMessage.author.username.match("ChaseDunton")) {
+      const chaseReactProbability = Math.floor(Math.random() * 10);
+      if (chaseReactProbability <= 2) {
+        receivedMessage.react(chaseBabyEmote);
+      }
     }
-  }
 
-  if (
-    receivedMessage.author.username.match("ChaseDunton") &&
-    incomingM.startsWith("make ")
-  ) {
-    receivedMessage.channel.send("Make it yourself.");
-  }
-
-  if (
-    incomingM.startsWith("vaux what are you doing") ||
-    incomingM.startsWith("vaux wyd")
-  ) {
-    const activities = client.user.presence.activities;
-    let botActivities = [];
-    activities.forEach((activity) => {
-      botActivities.push(
-        activity.type.toLocaleLowerCase() + " " + activity.name
-      );
-    });
-    receivedMessage.channel.send(botActivities.toLocaleString());
-  }
-
-  // receivedMessage.guild.emojis.cache.forEach((customEmoji) => {
-  //   logChannelAndEmojiIDs(`${customEmoji.name} ${customEmoji.id}\n`);
-  // });
-
-  if (incomingM.includes("bella")) {
-    receivedMessage.react(bellaEmote);
-  }
-
-  if (incomingM.includes("daddy")) {
-    receivedMessage.react(daddyEmote);
-  }
-
-  if (incomingM.startsWith("vaux say hi")) {
-    receivedMessage.channel.send(
-      "Yo yo what up " + receivedMessage.author.toString() + "!",
-      { tts: true }
-    );
-    localCommandCountDataWriteService("sayHi");
-  }
-
-  if (incomingM.startsWith("vaux truth")) {
-    receivedMessage.channel.send("Half Life 3 will never be made.");
-    localCommandCountDataWriteService("truth");
-  }
-
-  if (incomingM.startsWith("vaux what's good")) {
-    receivedMessage.channel.send(
-      "https://www.youtube.com/watch?v=0tdyU_gW6WE&ab_channel=NeilCicierega"
-    );
-    localCommandCountDataWriteService("whatsGood");
-  }
-
-  if (incomingM.startsWith("vaux fortnite")) {
-    const fnGifArr = [
-      "https://media.giphy.com/media/jO2NwLwjQ3zfntm5Qm/giphy.gif",
-      "https://media.giphy.com/media/kEWaRPxjebqskhawMt/giphy.gif",
-      "https://media.giphy.com/media/TDKxZPossicXJEm455/giphy.gif",
-      "https://media.giphy.com/media/iMBEVqJRJkEQC5EGa2/giphy.gif",
-      "https://media.giphy.com/media/fefVaYmApHUdOjj1o6/giphy.gif",
-      "https://media.giphy.com/media/878n18Z0gJNAUXpubj/giphy.gif",
-      "https://media.giphy.com/media/1APhATvqD65r966yCP/giphy.gif",
-      "https://media.giphy.com/media/W5lF9jj0dkU56bQTHF/giphy.gif",
-    ];
-    const randomGifIndex = Math.floor(Math.random() * fnGifArr.length);
-    receivedMessage.channel.send(fnGifArr[randomGifIndex]);
-    if (incomingM.includes("count")) {
-      receivedMessage.channel.send(fnGifArr.length);
+    if (incomingM.includes("bella")) {
+      receivedMessage.react(bellaEmote);
     }
-    localCommandCountDataWriteService("fortnite");
-  }
 
-  if (
-    incomingM.startsWith("vaux command hit execution count") ||
-    incomingM.match("^vaux chec$")
-  ) {
-    const commandCountObj = localCommandCountDataReadService();
-    let commandCountMessage =
-      "Thanks: " +
-      commandCountObj.commandCount.thanks +
-      "\nTruth: " +
-      commandCountObj.commandCount.truth +
-      "\nSay Hi: " +
-      commandCountObj.commandCount.sayHi +
-      "\nHelp: " +
-      commandCountObj.commandCount.help +
-      "\nFortnite: " +
-      commandCountObj.commandCount.fortnite +
-      "\n✅CHEC: " +
-      commandCountObj.commandCount.chec +
-      "\nWhat's Good: " +
-      commandCountObj.commandCount.whatsGood;
-    receivedMessage.channel.send(commandCountMessage);
-    localCommandCountDataWriteService("chec");
-  }
+    if (incomingM.includes("daddy")) {
+      receivedMessage.react(daddyEmote);
+    }
 
-  if (
-    incomingM.includes("thanks") ||
-    (incomingM.includes("thank you") && incomingM.includes("vaux"))
-  ) {
-    receivedMessage.react("👍");
-    localCommandCountDataWriteService("thanks");
-  }
+    // All commands Vaux's channel only
+    if (receivedMessage.channel.id === "755615150299676752") {
+      if (receivedMessage.author.username.match("LN")) {
+        const tadaReactProbability = Math.floor(Math.random() * 10);
+        if (tadaReactProbability === 0) {
+          receivedMessage.react("🎉");
+        }
+      }
 
-  if (incomingM.startsWith("vaux help")) {
-    receivedMessage.channel.send(
-      "Command prefix: Vaux\n" +
-        "Current commands:\n" +
-        "[Help] - displays this text\n" +
-        "[Fortnite] - displays the folly of humanity\n" +
-        "[Truth] - displays today's truth\n" +
-        "[What's Good] - Bustin\n" +
-        "[Say Hi] - Vaux says hello\n \n" +
-        "All commands are currently WIP and restricted to Vaux's channel."
-    );
-    localCommandCountDataWriteService("help");
+      // receivedMessage.guild.emojis.cache.forEach((customEmoji) => {
+      //   logChannelAndEmojiIDs(`${customEmoji.name} ${customEmoji.id}\n`);
+      // });
+
+      if (incomingM.startsWith("vaux say hi")) {
+        receivedMessage.channel.send(
+          "Yo yo what up " + receivedMessage.author.toString() + "!",
+          { tts: true }
+        );
+        localCommandCountDataWriteService("sayHi");
+      }
+
+      if (incomingM.startsWith("vaux truth")) {
+        receivedMessage.channel.send("Half Life 3 will never be made.");
+        localCommandCountDataWriteService("truth");
+      }
+
+      if (incomingM.startsWith("vaux what's good")) {
+        receivedMessage.channel.send(
+          "https://www.youtube.com/watch?v=0tdyU_gW6WE&ab_channel=NeilCicierega"
+        );
+        localCommandCountDataWriteService("whatsGood");
+      }
+
+      if (incomingM.startsWith("vaux fortnite")) {
+        const fnGifArr = [
+          "https://media.giphy.com/media/jO2NwLwjQ3zfntm5Qm/giphy.gif",
+          "https://media.giphy.com/media/kEWaRPxjebqskhawMt/giphy.gif",
+          "https://media.giphy.com/media/TDKxZPossicXJEm455/giphy.gif",
+          "https://media.giphy.com/media/iMBEVqJRJkEQC5EGa2/giphy.gif",
+          "https://media.giphy.com/media/fefVaYmApHUdOjj1o6/giphy.gif",
+          "https://media.giphy.com/media/878n18Z0gJNAUXpubj/giphy.gif",
+          "https://media.giphy.com/media/1APhATvqD65r966yCP/giphy.gif",
+          "https://media.giphy.com/media/W5lF9jj0dkU56bQTHF/giphy.gif",
+        ];
+        const randomGifIndex = Math.floor(Math.random() * fnGifArr.length);
+        receivedMessage.channel.send(fnGifArr[randomGifIndex]);
+        if (incomingM.includes("count")) {
+          receivedMessage.channel.send(fnGifArr.length);
+        }
+        localCommandCountDataWriteService("fortnite");
+      }
+
+      if (
+        incomingM.startsWith("vaux command hit execution count") ||
+        incomingM.match("^vaux chec$")
+      ) {
+        const commandCountObj = localCommandCountDataReadService();
+        let commandCountMessage =
+          "Thanks: " +
+          commandCountObj.commandCount.thanks +
+          "\nTruth: " +
+          commandCountObj.commandCount.truth +
+          "\nSay Hi: " +
+          commandCountObj.commandCount.sayHi +
+          "\nHelp: " +
+          commandCountObj.commandCount.help +
+          "\nFortnite: " +
+          commandCountObj.commandCount.fortnite +
+          "\n✅CHEC: " +
+          commandCountObj.commandCount.chec +
+          "\nWhat's Good: " +
+          commandCountObj.commandCount.whatsGood;
+        receivedMessage.channel.send(commandCountMessage);
+        localCommandCountDataWriteService("chec");
+      }
+
+      if (
+        incomingM.includes("thanks") ||
+        (incomingM.includes("thank you") && incomingM.includes("vaux"))
+      ) {
+        receivedMessage.react("👍");
+        localCommandCountDataWriteService("thanks");
+      }
+
+      if (incomingM.startsWith("vaux help")) {
+        receivedMessage.channel.send(
+          "Command prefix: Vaux\n" +
+            "Current commands:\n" +
+            "[Help] - displays this text\n" +
+            "[Fortnite] - displays the folly of humanity\n" +
+            "[Truth] - displays today's truth\n" +
+            "[What's Good] - Bustin\n" +
+            "[Say Hi] - Vaux says hello\n \n" +
+            "All commands are currently WIP and restricted to Vaux's channel."
+        );
+        localCommandCountDataWriteService("help");
+      }
+    }
+    return;
   }
 });
 
